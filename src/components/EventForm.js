fb193f141bb87c23d2438af937040c40c6aeb967
kkata <14849535+kkata@users.noreply.github.com>
2020-01-08 14:45:17 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,12 +1,19 @@
 import React, { useContext, useState } from 'react'
 
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions'
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext)
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
+
   const addEvent = (e) => {
     e.preventDefault()
     dispatch({
@@ -14,13 +21,25 @@ const EventForm = () => {
       title,
       body
     })
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    })
     setTitle('')
     setBody('')
   }
   const deleteAllEvents = (e) => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({ type: DELETE_ALL_EVENTS })
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS })
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
   const unCreatable = title === '' || body === ''
 
